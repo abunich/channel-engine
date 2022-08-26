fbfacf06257f035f5437846a2ee2619a16f5797f
Andrei Shatrov <andrey.shatrov@akvelon.com>
2022-08-26 19:03:43 +0300

    Created the utils folder to store urls and paths

---
+++ src/utils/paths.ts
@@ -0,0 +1,3 @@
+export const OVERVIEW_URL = "/";
+
+export const DETAILS_URL = "/details";
---
+++ src/utils/urls.ts
@@ -0,0 +1,4 @@
+
+const API_URL = "https://api-dev.channelengine.net/api/v2";
+
+export const ORDERS_URL = `${API_URL}/orders`
