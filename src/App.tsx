143adacf1dbf988bb5cd56719fec402e6161030a
Andrei Shatrov <andrey.shatrov@akvelon.com>
2022-08-26 19:06:03 +0300

    Router has been connected to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Overview } from "./pages/Overview/Overview";
+import { Details } from "./pages/Details/Details";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { OVERVIEW_URL, DETAILS_URL } from "./utils/paths";
 import "./App.scss";
 
 const App: React.FC = () => {
-  return <div>Channel Engine</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path={OVERVIEW_URL} element={<Overview />} />
+            <Route path={DETAILS_URL} element={<Details />} />
+          </Routes>
+        </BrowserRouter>
+        ,
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
