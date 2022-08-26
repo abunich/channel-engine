178ec875fbf31ddfd72394aabc9cbcb8fab5311f
Andrei Shatrov <andrey.shatrov@akvelon.com>
2022-08-26 20:35:58 +0300

    Getting order data in the Overview page

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useOrders } from "src/hooks/useOrders";
 
 export const Overview: React.FC = () => {
+  const data = useOrders();
+
+  console.log("data", data);
   return <div>Overview</div>;
 };
